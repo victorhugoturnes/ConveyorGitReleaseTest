6bc79a11513da43adb252ff4f85c4cfb30b063f2
Victor Hugo <victorhugoturnes@hotmail.com>
2023-04-07 14:44:01 -0300

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://maven.hq.hydraulic.software")
+    }
+}
+
+
+
+rootProject.name = "ConveyorGitReleaseTest"
+
