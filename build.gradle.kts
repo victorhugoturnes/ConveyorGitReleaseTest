2f828a0ac4ad2142bb4ded4330c9dd96fa2c2d5a
Victor Hugo <victorhugoturnes@hotmail.com>
2023-04-07 15:29:02 -0300

    Update Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.example"
-version = "1.0.0"
+version = "1.0.1"
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(19))
 compose.desktop.application.mainClass = "MainKt"
 
