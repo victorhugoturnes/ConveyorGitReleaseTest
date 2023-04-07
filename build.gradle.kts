077f20a1156280c2b06bc42bc6c9f993f1c1a809
Victor Hugo <victorhugoturnes@hotmail.com>
2023-04-07 17:05:03 -0300

    Only publish on master push

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.example"
-version = "1.0.2"
+version = "1.0.3"
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(19))
 compose.desktop.application.mainClass = "MainKt"
 
