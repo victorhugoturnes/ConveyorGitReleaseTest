942b280ba9eb996bf7b17b52f8d118a5d378fe3a
Victor Hugo <victorhugoturnes@hotmail.com>
2023-04-08 11:13:19 -0300

    Adding an icon

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,9 +7,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.nio.file.Path
 import java.util.*
+import kotlin.io.path.exists
+import kotlin.io.path.inputStream
 
 @Composable
 @Preview
@@ -29,7 +35,24 @@ fun App() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication,
+        title = "Conveyor git release test environment",
+        icon = appIcon) {
         App()
+    }
+}
+
+private val appIcon: Painter? by lazy {
+    // app.dir is set when packaged to point at our collected inputs.
+    val appDirProp = System.getProperty("app.dir")
+    val appDir = appDirProp?.let { Path.of(it) }
+    // On Windows we should use the .ico file. On Linux, there's no native compound image format and Compose can't render SVG icons,
+    // so we pick the 128x128 icon and let the frameworks/desktop environment rescale. On macOS, we don't need to do anything.
+    var iconPath = appDir?.resolve("app.ico")?.takeIf { it.exists() }
+    iconPath = iconPath ?: appDir?.resolve("icon-square-128.png")?.takeIf { it.exists() }
+    if (iconPath?.exists() == true) {
+        BitmapPainter(iconPath.inputStream().buffered().use { loadImageBitmap(it) })
+    } else {
+        null
     }
 }
